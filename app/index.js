4a0820e23e46c8c361d1632651d1abf148898ec9
Andrew Qu <qua@rpi.edu>
2019-07-23 18:56:46 -0400

    created sample accelerometer display to test text placements

--- app/index.js
+++ app/index.js
@@ -1,9 +1,22 @@
+let document = require("document")
 import { Accelerometer } from "accelerometer";
 
 if (Accelerometer) {
   // sampling at 1Hz (once per second)
   const accel = new Accelerometer({ frequency: 1 });
   accel.addEventListener("reading", () => {
+
+    var x = document.getElementById("x")
+    var y = document.getElementById("y")
+    var z = document.getElementById("z")
+
+    x.text= accel.x
+    y.text= accel.y
+    z.text= accel.z
+
+
+    // make socket connection with companion
+
     console.log(
       `ts: ${accel.timestamp}, \
        x: ${accel.x}, \
--- companion/index.js
+++ companion/index.js
@@ -1,1 +1,23 @@
 console.log('Hello world!');
+/*
+    fetch('https://shuttles.rpi.edu/updates')
+      .then(function(response){
+        return reponse.json();
+      })
+      .then(function(json){
+        shuttle.text = json
+      });
+
+*/
+
+/*
+ *
+ *  establish a socket connection with device
+ *
+ *  make fetch request to shuttles rpi
+ *
+ *  send data to device
+ *
+ *
+ *
+ */
